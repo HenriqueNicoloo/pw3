fd4c20dcd82b03a7ad6ae8fe4baa3512fc8185a1
Emilly.R-Henrique.N <Emilly.R-Henrique.N@test.com>
2023-03-07 10:46:16 -0300

    Projeto biscoito da sorte - PW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'error 404';
-  logo = "favicon.ico";
- // pet = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fimage-photo%2Fyorkshire-terrier-golddust-on-white-401648239&psig=AOvVaw1_6rYmtPX3b74MvaMUogcY&ust=1677673185776000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCOCIi_CZuP0CFQAAAAAdAAAAABAD"
-
-
-
-  obterAutor(){
-    return "Henrique da Silva";
-  }
-
-  nome = 'Henrique';
-  sobrenome = 'Rodrigues';
-  idade = '34';
-  nomeCompleto(){
-    return this.nome + ' ' + this.sobrenome;
-  }
-
-  obterAlerta(){
-    alert("Te amo");
-  }
+  biscoitoInteiro = "assets/biscoitoInteiro.png";
+  biscoitoQuebrado = "assets/biscoitoQuebrado.png";
+  mensagem = [
+    "Se a terra é o terceiro planeta do sistema solar, então todos os países deveriam ser considerados de terceiro mundo?",
+    "A vida não vai conseguir te derrubar com você deitado",
+    "A vingança nunca é plena, mata a alma e a envenena",
+    "Se você derrubar sabão no chão, ele ta limpo ou ta sujo?"];
 
   ocultar = true;
-  obterMsg(){
-    this.ocultar = !this.ocultar;
+
+  ocultarMensagem(){
+    this.ocultar = false;
+  }
+
+  sortearFrases(){
+    return this.mensagem[Math.floor(Math.random() * this.mensagem.length)];
   }
 }
 
